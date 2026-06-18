2c5752c8e325058bbd1a8836a69e9165316bb67a
YC <ug000sx4@gmail.com>
2026-06-18 13:27:18 +0200

    First commit

---
+++ src/services/post.service.js
@@ -0,0 +1,35 @@
+export const createPost = async (author, data) => {
+    // TODO
+}
+
+export const getPostById = async (id) => {
+    // TODO
+}
+
+export const deletePost = async (id) => {
+    // TODO
+}
+
+export const addLike = async (id) => {
+    // TODO
+}
+
+export const getPostsByAuthor = async (author) => {
+    // TODO
+}
+
+export const addComment = async (id, commenter, content) => {
+    // TODO
+}
+
+export const getPostsByTags = async (tagsString) => {
+    // TODO
+}
+
+export const getPostsByPeriod = async (dateFrom, dateTo) => {
+    // TODO
+}
+
+export const updatePost = async (id, data) => {
+    // TODO
+}
