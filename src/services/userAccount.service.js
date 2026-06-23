8972554d6a0fb2d0def32642b67a0de85ea98ab6
YC <ug000sx4@gmail.com>
2026-06-23 10:57:31 +0200

    Add userAccount.service with placeholder methods for user management functions

---
+++ src/services/userAccount.service.js
@@ -0,0 +1,23 @@
+export const register = async (user) => {
+    // TODO: Implement user registration logic
+}
+
+export const removeUser = async (login) => {
+    // TODO: Implement user removal logic
+}
+
+export const updateUser = async (login, updateData) => {
+    // TODO: Implement user update logic
+}
+
+export const changeRoles = async (login, role, isAddRole) => {
+    // TODO: Implement user role change logic
+}
+
+export const changePassword = async (login, newPassword) => {
+    // TODO: Implement user password change logic
+}
+
+export const getUser = async (login) => {
+    // TODO: Implement user retrieval logic
+}
