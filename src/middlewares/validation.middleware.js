7a8e247775f3daaad60479f7b7eba1b89a68514a
YC <ug000sx4@gmail.com>
2026-06-23 10:24:18 +0200

    Integrate validation middleware into post.routes and add schemas for addComment, updatePost, and getPostsByPeriod

--- src/middlewares/validation.middleware.js
+++ src/middlewares/validation.middleware.js
@@ -1,26 +1,41 @@
 import Joi from 'joi';
 
-const schema = {
+const schemas = {
     createPost: Joi.object({
-        title: Joi.string().trim().required(),
-        content: Joi.string().trim().required(),
-        tags: Joi.array().items(Joi.string().trim())
+        title: Joi.string().required(),
+        content: Joi.string().required(),
+        tags: Joi.array().items(Joi.string())
     }),
     addComment: Joi.object({
-        message: Joi.string().trim().required()
-    }),
-    getPostsByTags: Joi.object({
-        // tags: Joi.string().trim().required(),
-        // tags: Joi.array().items(Joi.string().trim())
-        tags: Joi.alternatives().try(Joi.string().trim(), Joi.array().items(Joi.string().trim()))
-    }),
-    getPostsByPeriod: Joi.object({
-        dateFrom: Joi.date().required(),
-        dateTo: Joi.date().required()
+        message: Joi.string().required()
     }),
     updatePost: Joi.object({
-        title: Joi.string().trim(),
-        content: Joi.string().trim(),
-        tags: Joi.array().items(Joi.string().trim())
+        title: Joi.string(),
+        content: Joi.string(),
+        tags: Joi.array().items(Joi.string())
     }),
+    dateFormatPeriod: Joi.object({
+        dateFrom: Joi.date().iso().required(),
+        dateTo: Joi.date().iso().required().greater(Joi.ref('dateFrom'))
+    })
 }
+
+const validate = (schemaName, target = 'body') => (req, res, next) => {
+    const schema = schemas[schemaName];
+    if (!schema) {
+        return next(new Error('Invalid schema name'));
+    }
+    const {error} = schema.validate(req[target]);
+    if (error) {
+        return res.status(400).json({
+            message: error.details[0].message,
+            code: 400,
+            status: 'Bad Request',
+            timestamp: new Date().toISOString(),
+            path: req.path
+        });
+    }
+    return next();
+}
+
+export default validate;
--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -1,16 +1,17 @@
 import { Router } from 'express';
 import {createPost,getPostById,deletePost,updatePost,addComment,addLike,getPostsByTags,getPostsByPeriod,getPostsByAuthor,} from '../controllers/post.controller.js';
+import validate from "../middlewares/validation.middleware.js";
 
 const router = Router();
 
-router.post('/post/:author', createPost);
+router.post('/post/:author', validate('createPost'), createPost);
 router.get('/post/:id', getPostById);
 router.delete('/post/:id', deletePost);
 router.patch('/post/:id/like', addLike);
 router.get('/posts/author/:author', getPostsByAuthor);
-router.patch('/post/:id/comment/:commenter', addComment);
+router.patch('/post/:id/comment/:commenter', validate('addComment'), addComment);
 router.get('/posts/tags', getPostsByTags);
-router.get('/posts/period', getPostsByPeriod);
-router.patch('/post/:id', updatePost);
+router.get('/posts/period', validate('dateFormatPeriod', 'query'), getPostsByPeriod);
+router.patch('/post/:id', validate('updatePost'), updatePost);
 
 export default router;
