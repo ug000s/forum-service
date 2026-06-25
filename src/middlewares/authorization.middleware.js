ac96782f664824b7e64cd05205492b81db98c69b
YC <ug000sx4@gmail.com>
2026-06-25 10:41:38 +0200

    Add isOwnerOrHasRole middleware and update routes to support combined ownership and role-based access control

--- src/middlewares/authorization.middleware.js
+++ src/middlewares/authorization.middleware.js
@@ -1,3 +1,9 @@
 export const hasRole = role => (req, res, next) => req.principal.roles.includes(role.toUpperCase().trim())  ? next() : res.status(403).json({message: 'Access denied'});
 
 export const isOwner = paramName => (req, res, next) => req.params[paramName] === req.principal.userName ? next() : res.status(403).json({message: 'Access denied'});
+
+export const isOwnerOrHasRole = (paramName, role ) => (req, res, next) => {
+    const isOwner = req.params[paramName] === req.principal.userName;
+    const hasRole = req.principal.roles.includes(role.toUpperCase().trim());
+    return isOwner || hasRole ? next() : res.status(403).json({message: 'Access denied'});
+}
--- src/routes/authorization.routes.js
+++ src/routes/authorization.routes.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import {hasRole, isOwner} from "../middlewares/authorization.middleware.js";
+import {hasRole, isOwner, isOwnerOrHasRole} from "../middlewares/authorization.middleware.js";
 import {ADMIN} from "../configuration/constants.js";
 
 const router = Router();
 
 router.all('/account/user/:login/role/:role', hasRole(ADMIN));
 router.patch('/account/user/:user', isOwner('user'));
+router.delete('/account/user/:login', isOwnerOrHasRole('login', ADMIN));
 
 export default router;
