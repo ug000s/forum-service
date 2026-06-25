4c3300928ab9eb74d53f0afe6fcc1585b5af2578
YC <ug000sx4@gmail.com>
2026-06-25 10:22:11 +0200

    Add isOwner middleware and update routes for user-specific access control

--- src/middlewares/authorization.middleware.js
+++ src/middlewares/authorization.middleware.js
@@ -1,1 +1,3 @@
 export const hasRole = role => (req, res, next) => req.principal.roles.includes(role.toUpperCase().trim())  ? next() : res.status(403).json({message: 'Access denied'});
+
+export const isOwner = paramName => (req, res, next) => req.params[paramName] === req.principal.userName ? next() : res.status(403).json({message: 'Access denied'});
--- src/routes/authorization.routes.js
+++ src/routes/authorization.routes.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import {hasRole} from "../middlewares/authorization.middleware.js";
+import {hasRole, isOwner} from "../middlewares/authorization.middleware.js";
 import {ADMIN} from "../configuration/constants.js";
 
 const router = Router();
 
 router.all('/account/user/:login/role/:role', hasRole(ADMIN));
+router.patch('/account/user/:user', isOwner('user'));
 
 export default router;
